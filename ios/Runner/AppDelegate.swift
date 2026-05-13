8ffcb3a6e91c6099a702185630f0b408e7ea0206
metla <r69314272@gmail.com>
2026-05-13 19:51:19 +0300

    fix: move LiquidGlass into AppDelegate, fix

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -12,8 +12,48 @@ import UIKit
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
-    let registrar = engineBridge.pluginRegistry.registrar(forPlugin: "LiquidGlassPlugin")
+    guard let registrar = engineBridge.pluginRegistry.registrar(forPlugin: "LiquidGlassPlugin") else {
+      return
+    }
     let factory = LiquidGlassViewFactory(messenger: registrar.messenger())
     registrar.register(factory, withId: "liquid_glass_view")
   }
+}
+
+// MARK: - Liquid Glass Platform View
+
+class LiquidGlassViewFactory: NSObject, FlutterPlatformViewFactory {
+  private let messenger: FlutterBinaryMessenger
+
+  init(messenger: FlutterBinaryMessenger) {
+    self.messenger = messenger
+    super.init()
+  }
+
+  func create(withFrame frame: CGRect, viewIdentifier viewId: Int64, arguments args: Any?) -> FlutterPlatformView {
+    return LiquidGlassView(frame: frame)
+  }
+
+  func createArgsCodec() -> FlutterMessageCodec & NSObjectProtocol {
+    return FlutterStandardMessageCodec.sharedInstance()
+  }
+}
+
+class LiquidGlassView: NSObject, FlutterPlatformView {
+  private let container: UIView
+
+  init(frame: CGRect) {
+    container = UIView(frame: frame)
+    container.backgroundColor = .clear
+    container.isOpaque = false
+    super.init()
+
+    let blur = UIBlurEffect(style: .systemUltraThinMaterialDark)
+    let blurView = UIVisualEffectView(effect: blur)
+    blurView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    blurView.frame = container.bounds
+    container.addSubview(blurView)
+  }
+
+  func view() -> UIView { container }
 }
